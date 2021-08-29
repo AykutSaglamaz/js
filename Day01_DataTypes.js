30b97173e83cc74c6fe020daaa9f36ffd4862b1b
AygutSaglamaz <gulerrahim@gmail.com>
2021-08-29 16:43:35 -0400

    first commit

---
+++ Day01_DataTypes.js
@@ -0,0 +1,80 @@
+console.clear();
+
+/*
+Primitive Data Type in JS: 
+string, number, boolean, null, undefined
+*/
+var name = 'javascript';
+console.log(typeof(name));//"string"
+
+var myName ="java"
+
+console.log(typeof(name));//"string"
+
+/*
+Number literal:
+we only have number data type. No int, double, byte
+*/
+
+var myNum=20;
+console.log(typeof(myNum));//"number"
+
+var myNum=20.89;
+console.log(typeof(myNum));//"number"
+
+var myNum=20/0;
+console.log(myNum);//Infinity
+console.log(typeof(myNum));//"number"
+
+//number+string
+
+var num1 =20+"a";
+console.log(num1);//"20a"
+
+console.log(typeof(num1));//"string"
+
+
+
+var num2 ="a"+20;
+console.log(num1);//"20a"
+
+console.log(typeof(num1));//"string"
+
+/*
+boolean: True or False
+
+*/
+
+var isExist=true
+console.log(isExist);//true
+console.log(typeof(isExist));//"boolean"
+
+
+var isExist=false
+console.log(isExist);//false
+console.log(typeof(isExist));//"boolean"
+
+
+/*
+null
+*/
+
+var pickedColor =null;
+console.log(pickedColor);//null
+console.log(typeof(pickedColor));//"object"
+
+
+/*
+Undefined
+*/
+
+var selectedCar;
+
+console.log(selectedCar);//undefined
+console.log(typeof(selectedCar));//"undefined"
+
+
+
+var selectedCar= undefined;
+console.log(selectedCar);//undefined
+console.log(typeof(selectedCar));//"undefined"
